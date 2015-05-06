2f7a48098ae4c2fc7a02c87b02e09293fb0f3875
F. Gabriel Gosselin <gabriel@evidens.ca>
2015-05-05 23:56:49 -0700

    Adding a bit about counting loops, finding sub-strings and an exercise

---
+++ exercises/demo_loop_count.py
@@ -0,0 +1,10 @@
+import csv
+
+csv_file = open('../exercises/llc-chapters.csv')
+csv_data = csv.DictReader(csv_file)
+
+count = 0
+for row in csv_data:
+    count += 1
+
+print("There are " + str(count) + " chapters")
