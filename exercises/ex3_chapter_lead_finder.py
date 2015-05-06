3cb6b4ac9631f54e3a42afdd4df56676d31f1a6d
F. Gabriel Gosselin <gabriel@evidens.ca>
2015-05-05 22:24:23 -0700

    Add chapter lead finder exercise

---
+++ exercises/ex3_chapter_lead_finder.py
@@ -0,0 +1,12 @@
+# Import the csv library
+import csv
+
+# Open the 'llc-chapters.csv' file
+
+# Convert it to a csv_data structure
+
+# Loop through each of the rows
+
+    # Compare the 'City' in the row with your city
+
+        # Print "Thank you [[Chapter Lead(s)]]!"
