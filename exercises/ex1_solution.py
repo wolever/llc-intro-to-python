60955c78a060f28c304c8f99bd747bf3f96a18df
David Wolever <david@wolever.net>
2016-01-25 01:58:59 -0500

    Fix notes, start with edits

---
+++ exercises/ex1_solution.py
@@ -0,0 +1,6 @@
+# Assign the raw_input value to a variable
+# Your program waits for the user to hit enter
+name = raw_input("Please enter your name: ")
+
+# ... then continues to this line
+print("Hello " + name)
--- exercises/ex3_smarter_weather.py
+++ exercises/ex3_smarter_weather.py
@@ -1,12 +1,9 @@
-
 # Remember, we get a string from raw_input, but we need an int to compare it
 weather = int(raw_input("What temperature is it now? (as a number, please): "))
 
-# if the weather is greater than or equal to 25 degrees
-if "replace this with the right condition":
+if "the weather is greater than or equal to 25 degrees":
     print("Go to the beach!")
-# the weather is less than 25 degrees AND greater than 15 degrees
-elif "replace this with the right conditions":
+elif "weather is less than 25 degrees AND greater than 15 degrees":
     # Still warm enough for ice cream!
 else:
     # Wear a sweater and dream of beaches.
