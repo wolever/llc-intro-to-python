dd5fac17043747406cafb6700801eec4ace56381
Jacqueline Smith <jsmith.uoft@gmail.com>
2016-01-30 00:18:55 -0500

    updates to sandbox

--- exercises/sandbox.py
+++ exercises/sandbox.py
@@ -1,9 +1,1 @@
-weather_file = open('big_weather.csv')
-month_counts = {}
-for line in weather_file:
-    month = line.strip().split(',')[1]
-    if month in month_counts:
-        month_counts[month] += 1
-    else:
-        month_counts[month] = 1
-print month_counts
+print("Hello world!")
