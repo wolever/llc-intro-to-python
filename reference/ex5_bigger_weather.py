a35942ccb65e07e2a12b3e3da57a6a49aa9f846f
David Wolever <david@wolever.net>
2016-03-22 16:39:45 -0400

    Updates

--- reference/ex5_bigger_weather.py
+++ reference/ex5_bigger_weather.py
@@ -2,10 +2,10 @@ weather_file = open("../exercises/big_weather.csv")
 
 for line in weather_file:
     line_data = line.split(",")
-    weather = int(line_data[3])
-    if weather >= 25:
+    temperature = float(line_data[3])
+    if temperature >= 25:
         print(line + "Go to the beach!")
-    elif weather < 25 and weather > 15:
+    elif temperature < 25 and temperature > 15:
         print(line + "Still warm enough for ice cream!")
     else:
         print(line + "Wear a sweater and dream of beaches.")    
